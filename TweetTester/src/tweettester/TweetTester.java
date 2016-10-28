c54ab022b6299fafcd89e27d97cf40d53828ff4f
daranzhao <17zhaod@smtexas.org>
2016-10-28 13:45:40 -0500

    TweetTester to return string length.

--- TweetTester/src/tweettester/TweetTester.java
+++ TweetTester/src/tweettester/TweetTester.java
@@ -9,18 +9,33 @@ import java.util.Scanner;
 import java.lang.Math; 
 /**
  * @class  AP CSA
- * @author INSERT YOUR NAME HERE
+ * @author DARAN ZHAO
  */
 public class TweetTester {
 
     /**
      * @param args the command line arguments
+     * @param scan is the name of the keyboard
+     * @param tweet is the name of the String that stores the user input
      */
     public static void main(String[] args) {
         
         Scanner scan = new Scanner (System.in);
-         String tweet;
-         
+        //initialize String
+        String tweet;
+        
+        //ask user for String
+        System.out.print("Enter your tweet : ");
+		tweet = scan.nextLine();
+		
+		//return length and error if too long
+		if(tweet.length()<=140) {
+		    System.out.println("Your tweet is " + tweet.length() + " characters long.");
+		    	
+		}
+		else {
+			System.out.println("Your tweet is " + tweet.length() + " characters long. Sorry, that's too long!");
+		}
     }
     
 }
